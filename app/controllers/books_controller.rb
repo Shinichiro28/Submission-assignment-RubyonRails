102da505b496c45d3d1aa6fa15506ecf6ed9bcf6
吉田　慎一郎 <shinichiroyoshida@yoshidashinichirounoMacBook-Pro.local>
2022-04-13 23:05:42 +0900

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def create
+    @book=Book.new(book_params)
+    if @book.save
+      #保存成功
+      flash[:success]="Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      #保存失敗
+      @books=Book.all
+      render"index"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book=Book.new
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      #更新成功
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      #更新失敗
+      @books=Book.all
+      render "edit"
+    end
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
